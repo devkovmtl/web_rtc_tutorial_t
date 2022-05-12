fff1dd10b280953978e8423e2acd03ef854bf26e
Mikael <dev.kov.mtl@gmail.com>
2022-05-12 17:16:31 -0400

    PeerChat - Mic & Camera Controls

--- PeerChat/main.js
+++ PeerChat/main.js
@@ -202,6 +202,37 @@ let leaveChannel = async () => {
   await client.logout();
 };
 
+let toggleCamera = async () => {
+  let videoTrack = localStream
+    .getTracks()
+    .find((track) => track.kind === 'video');
+
+  if (videoTrack.enabled) {
+    videoTrack.enabled = false;
+    document.getElementById('camera-btn').style.backgroundColor =
+      'rgb(255,80,80)';
+  } else {
+    videoTrack.enabled = true;
+    document.getElementById('camera-btn').style.backgroundColor =
+      'rgba(179, 102,249,.9)';
+  }
+};
+
+let toggleMic = async () => {
+  let audioTrack = localStream
+    .getTracks()
+    .find((track) => track.kind === 'audio');
+
+  if (audioTrack.enabled) {
+    audioTrack.enabled = false;
+    document.getElementById('mic-btn').style.backgroundColor = 'rgb(255,80,80)';
+  } else {
+    audioTrack.enabled = true;
+    document.getElementById('mic-btn').style.backgroundColor =
+      'rgba(179, 102,249,.9)';
+  }
+};
+
 window.addEventListener('beforeunload', leaveChannel);
 
 init();
